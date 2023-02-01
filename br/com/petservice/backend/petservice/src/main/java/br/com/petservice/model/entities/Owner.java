2702384050f385ed3ebcf7de9bfb76ac915d717e
Daniel Marques <daniel.marques@edu.unifil.br>
2023-02-01 15:19:58 -0300

    creating owner entity

---
+++ br/com/petservice/backend/petservice/src/main/java/br/com/petservice/model/entities/Owner.java
@@ -0,0 +1,32 @@
+package br.com.petservice.model.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_owner")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Owner implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50)
+    private String name;
+
+    @Column(length = 11)
+    private String main_phone;
+
+    @Column(length = 11)
+    private String emergency_phone;
+
+}
