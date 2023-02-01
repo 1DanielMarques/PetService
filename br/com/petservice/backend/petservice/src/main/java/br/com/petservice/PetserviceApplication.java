513cd7b86bc2e0af88c5802539f5b049efba40f2
Daniel Marques <daniel.marques@edu.unifil.br>
2023-02-01 14:42:46 -0300

    criando projeto backend e organizando pastas

---
+++ br/com/petservice/backend/petservice/src/main/java/br/com/petservice/PetserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.petservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetserviceApplication.class, args);
+	}
+
+}
---
+++ br/com/petservice/backend/petservice/src/test/java/br/com/petservice/PetserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.petservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
